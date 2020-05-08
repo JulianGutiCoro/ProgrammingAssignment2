28ba2490a3ea53c9e7e056d158f683a0d03aa628
Julián Gutiérrez <jc.gutierrezcoronel@gmail.com>
2020-05-07 21:41:38 -0500

    update 1
    solution up

--- cachematrix.R
+++ cachematrix.R
@@ -3,26 +3,26 @@
 
 ## Write a short comment describing this function
 
-# This function creates a special matrix. It stores a matrix and chaches its inverse.
-makeCacheMatrix <- function(x = matrix()) { # Creo la función que guarda las cosas en el caché
+## This function creates a special matrix. It stores a matrix and chaches its inverse.
+makeCacheMatrix <- function(x = matrix()) { ## Creo la función que guarda las cosas en el caché
       
       i <- NULL
-      set <- function(y) { # guarda la información en el cahcé
+      set <- function(y) { ## guarda la información en el cahcé
             x <<- y
             i <<- NULL
       } 
       get <- function() x
       setinverse <- function(inverse) i <<- inverse
       getinverse <- function() i
-      list(set = set, get = get, # Creo la lista que guardará las cosas en el caché
+      list(set = set, get = get, ## Creo la lista que guardará las cosas en el caché
            setinverse = setinverse,
            getinverse = getinverse)
 }
 
 
-# Write a short comment describing this function
+## Write a short comment describing this function
 
-# This function checks if the inverse function has already been calculated. If not, it  calculates de inverse.
+## This function checks if the inverse function has already been calculated. If not, it  calculates de inverse.
 cacheSolve <- function(x, ...) { ## Creo la función que resuelve la inversa de la matrix
       i <- x$getinverse()
       if(!is.null(i)) { ## Se busca la matriz en el caché
@@ -36,6 +36,6 @@ cacheSolve <- function(x, ...) { ## Creo la función que resuelve la inversa de 
 }
 
 
-B <- makeCacheMatrix(A) # Ejercicio de comprobación de las funciones
+B <- makeCacheMatrix(A) ## Ejercicio de comprobación de las funciones
 cacheSolve(B)
 cacheSolve(B)
